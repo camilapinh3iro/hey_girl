247e452092c7bd13acb0ef1dffe84285ce20a8e7
Camila Pinheiro <pinheirocamila49800@gmail.com>
2023-04-22 22:45:52 -0300

    Imagens da section 'they' sendo puxadas pelo json. Trabalhando no knowMore.js

---
+++ js/index.js
@@ -0,0 +1,38 @@
+'use strict'
+
+import { mulheres } from "./mulheres.js"
+
+const makePhotos = (woman) => {
+
+
+    const li = document.createElement('li')
+    li.classList.add('li-woman')
+    li.onclick = () => {
+        localStorage.setItem("id", woman.id)
+    }
+
+    const a = document.createElement('a')
+    a.classList.add('content-woman')
+    a.href = "../html/she.html"
+
+    const img = document.createElement('img')
+    img.classList.add('photo-woman')
+    img.src = woman.image
+
+
+
+    a.append(img)
+    li.append(a)
+
+    return li
+}
+
+
+const loadPhotos = () => {
+    const container = document.querySelector('.container-photos-womens')
+    const photos = mulheres.map(makePhotos)
+
+    container.replaceChildren(...photos)
+}
+
+loadPhotos()
--- js/mulheres.js
+++ js/mulheres.js
@@ -1,22 +1,25 @@
 export const mulheres = [
     {
+        "id": 1,
         "name": "Malala Yousafzai",
         "profissão": "Ativista paquistanesa",
         "biografia": [
             "Malala Yousafzai (1997) é uma militante dos direitos das crianças, uma jovem paquistanesa que foi vítima de um atentado por defender o direito das meninas de ir à escola. Com 17 anos, foi a mais jovem ganhadora do Prêmio Nobel da Paz."
         ],
-        "image": "https://www.colegiocope.com.br/imgs/755/495/images/saiba-quem-e-malala-yousafzai.jpg"
+        "image": "../img/image-malala.webp"
     },
     {
+        "id": 2,
         "name": "Valentina Tereshkova",
         "profissão": "Astronauta russa",
         "biografia": [
             "Valentina Tereshkova foi a uma astronauta russa que em 1963 foi a primeira mulher a viajar para o espaço.",
             "Conquistou reconhecimento após sua missão e ingressou na carreira política.Curiosamente, mesmo com o passar dos anos, Valentina ainda é a única mulher a ter realizado uma viagem espacial sozinha."
         ],
-        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/RIAN_archive_612748_Valentina_Tereshkova.jpg/280px-RIAN_archive_612748_Valentina_Tereshkova.jpg"
+        "image": "../img/image-valentina.jpg"
     },
     {
+        "id": 3,
         "name": "Rosa Parks",
         "profissão": "Ativista negra norte-americana",
         "biografia": [
@@ -26,9 +29,10 @@ export const mulheres = [
             "Devido a sua atitude, a ativista foi presa.Isso gerou indignação na população negra e impulsionou grandes protestos que resultaram na anulação das leis de segregação racial."
 
         ],
-        "image": "https://i.natgeofe.com/k/e05eb527-8776-4f74-bb4f-d17814d91a43/rosa-parks-portrait.jpg"
+        "image": "../img/image-rosa.jpg"
     },
     {
+        "id": 4,
         "name": "Frida Kahlo",
         "profissão": "Pintora mexicana",
         "biografia": [
@@ -36,9 +40,10 @@ export const mulheres = [
             "Teve uma produção intensa, pintando autorretratos e cenas surrealistas com forte identidade latino- americana.",
             "Atualmente, a artista é reconhecida também como um emblema feminista.Isso porque mesmo não se identificando como tal, teve uma postura marcada contra o sistema patriarcal e impôs suas ideias de forma criativa e decidida."
         ],
-        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyoOgOphCN9gVaPc5_MHrEeZFSFVmj7nP60g&usqp=CAU"
+        "image": "../img/image-frida.jpg"
     },
     {
+        "id": 5,
         "name": "Amelia Earhart",
         "profissão": "Pioneira",
         "biografia": [
@@ -47,31 +52,34 @@ export const mulheres = [
             "Amalia foi uma figura atuante para que outras mulheres pudessem também pilotar, pois contribuiu em organizações que tinham esse objetivo.",
             "Em 1937, ao tentar dar a volta ao mundo de avião, Amalia desapareceu.Assim, foi dada como morta oficialmente em 1939."
         ],
-        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgCy_UpQyn69hC4uhS3tr_7vIIKVzygKoSRbM0-KpDJupPsqQOpo2oF7-KlHrrhpX07XI&usqp=CAU"
+        "image": "../img/image-amelia.jpg"
     },
     {
+        "id": 6,
         "name": "Anne Frank",
         "profissão": "Símbolo contra a intolerância",
         "biografia": [
             "O Diário de Anne Frank revelou ao mundo uma das histórias mais desumanas dos últimos séculos e continua sendo utilizado como um importante material histórico sobre a luta contra qualquer tipo de preconceito. A família judia Frank viveu na Alemanha e na Áustria ao longo da ascensão de Hitler na Segunda Guerra Mundial. Através de um diário, a jovem revelou de maneira honesta e pungente os acontecimentos de sua vida antes de morrer em 1945. Seu pai foi o responsável por publicar o diário da jovem que se tornou uma das mais importantes figuras da história moderna."
         ],
-        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhCVvSG_itOGPPyTozLXg5JJa5Ujs5Oj3Wqw&usqp=CAU"
+        "image": "../img/image-anne.jpg"
     },
     {
+        "id": 7,
         "name": "Maria da Penha",
         "profissão": "Líder de movimentos de defesa dos direitos das mulheres",
         "biografia": [
             "A Lei Maria da Penha em defesa das vítimas de violência doméstica existe hoje no Brasil muito em virtude da luta desta mulher em garantir que o seu agressor fosse condenado. Maria da Penha foi vítima de graves agressões por parte do marido que a deixaram paraplégica. Isto não a impediu de agir em busca de justiça para o seu caso e de criar movimentos em defesa de outras vítimas.",
         ],
-        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTecp4Ys3x8wCFOTl96Mwo2CtykQumulFHnjtzg_-wcU7tPtk8Rn_egCWNv4gPJGCKz_og&usqp=CAU"
+        "image": "../img/image-maria.jpeg"
     },
     {
+        "id": 8,
         "name": "Marta Vieira",
         "profissão": "Jogadora de Futebol",
         "biografia": [
             "Marta Vieira da Silva, a Marta, é a principal jogadora de futebol do mundo e atua em campo na posição de atacante. Nascida no município de Dois Riachos, em Alagoas, no dia 19 de fevereiro de 1986, a brasileira é chamada de rainha do futebol e foi eleita seis vezes como a melhor jogadora do mundo pela Federação Internacional de Futebol (Fifa).",
             "A atleta teve uma infância humilde no interior de Alagoas.O pai deixou a família quando ela tinha apenas um ano de idade.Desde então, Marta e os três irmãos(Ângela, José e Valdir) foram criados pela mãe Tereza da Silva."
         ],
-        "image": "https://internacionaldaamazoniacom.files.wordpress.com/2022/05/82f5bcf0-b141-0137-5833-0a58a9feac2a-minified-edited.jpg"
+        "image": "../img/image-marta.jpg"
     }
 ]
